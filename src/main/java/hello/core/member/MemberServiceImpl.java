2e866e296a44ce9e0b71f51b281f048a41a8e941
w0nnie <kjw55523@gmail.com>
2023-03-07 19:01:19 +0900

    관심사의 분리

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -5,7 +5,11 @@ public class MemberServiceImpl implements MemberService {
 
     //DIP를 위반하고 있다
     //serviceImpl은 추상화, 구현체도 의존하고있다.
-    MemberRepository memberRepository = new MemoryMemberRepository();
+    MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,14 +3,21 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolichy;
 import hello.core.member.Member;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberServiceImpl;
+import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
 
-    private final MemberServiceImpl memberService = new MemberServiceImpl();
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
-    //    private final DiscountPolicy discountPolicy = new FixDiscountPolichy();
-    private DiscountPolicy discountPolicy;
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+
     //dip 위반 구현체가 아닌 추상화한 객체만 의존해야한다.
 
     @Override
